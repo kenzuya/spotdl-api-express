9340a5c4497e433c9833b9648fa053cd0b19f606
Setsu <pbgarena0838@gmail.com>
2022-09-04 17:55:27 +0700

    feat: include lyrics in audio fix: handle error

--- server.js
+++ server.js
@@ -41,37 +41,41 @@ async function main() {
         res.json(result.data)
     })
     app.post('/download', async (req, res) => {
-        const url = req.query.url.split('&')[0]
-        if(!url) res.json({"status": "failed", "reason": "please input link in query"})
-        const {data} = await axios({
-            url: `http://localhost:${spotdlPORT}/api/song/url?url=${encodeURIComponent(url)}`,
-            method: 'GET'
-        })
-        // const cache = JSON.parse(readFileSync('./cache/cache.json'))
-        const filename = `${data.artists.length > 0 ? data.artists.join(', '): data.artists[0]} - ${data.name}.mp3`
-        if (existsSync(`./public/${filename}`)) {
-            data.download_url = `${web_url}/${encodeURIComponent(filename)}`
-            res.json(data)
-        } else {
-            exec(`spotdl download ${url} --bitrate 320k --output public`, (err, stdout, stderr) => {
-                if (err) res.json(JSON.stringify({status: "failed", reason: "A unexpected error"}))
-                if (stdout) {
-                    if(stdout.includes('Skipping')) {
-                        const split = stdout.split('Skipping ')[1].replace('\r\n\r\n', "")
-                        const listDir = readdirSync('./public')
-                        const findMusic = listDir.find(file => file.includes(data.name))
-                        data.download_url = `${web_url}/${encodeURIComponent(findMusic)}`
-                        res.json(data)
-                    } else {
-                        const listDir = readdirSync('./public')
-                        const findMusic = listDir.find(file => file.includes(data.name))
-                        data.download_url = `${web_url}/${encodeURIComponent(findMusic)}`
-                        res.json(data)
-                    }
-                    
-                }
-                if(stderr) res.json(JSON.stringify({status: "failed", reason: "A unexpected error"}))
+        try {
+            const url = req.query.url.split('&')[0]
+            if(!url) res.json({"status": "failed", "reason": "please input link in query"})
+            const {data} = await axios({
+                url: `http://localhost:${spotdlPORT}/api/song/url?url=${encodeURIComponent(url)}`,
+                method: 'GET'
             })
+            // const cache = JSON.parse(readFileSync('./cache/cache.json'))
+            const filename = `${data.artists.length > 0 ? data.artists.join(', '): data.artists[0]} - ${data.name}.mp3`
+            if (existsSync(`./public/${filename}`)) {
+                data.download_url = `${web_url}/${encodeURIComponent(filename)}`
+                res.json(data)
+            } else {
+                exec(`spotdl download ${url} --bitrate 320k --output public --lyrics musixmatch`, (err, stdout, stderr) => {
+                    if (err) res.json(JSON.stringify({status: "failed", reason: "A unexpected error"}))
+                    if (stdout) {
+                        if(stdout.includes('Skipping')) {
+                            const split = stdout.split('Skipping ')[1].replace('\r\n\r\n', "")
+                            const listDir = readdirSync('./public')
+                            const findMusic = listDir.find(file => file.includes(data.name))
+                            data.download_url = `${web_url}/${encodeURIComponent(findMusic)}`
+                            res.json(data)
+                        } else {
+                            const listDir = readdirSync('./public')
+                            const findMusic = listDir.find(file => file.includes(data.name))
+                            data.download_url = `${web_url}/${encodeURIComponent(findMusic)}`
+                            res.json(data)
+                        }
+                        
+                    }
+                    if(stderr) res.json(JSON.stringify({status: "failed", reason: "A unexpected error"}))
+                })
+            }
+        } catch (err) {
+            res.json(JSON.stringify({status: "failed", reason: "A unexpected error"}))
         }
         
     })
